19938dd3116f2f9bb3742f932cb78cd1a61729e5
ia <ia@host.docker.internal>
2026-02-02 14:22:58 +0900

    2026_0202_1422_販売管理TOPページ遷移完成

--- src/main/java/com/example/demo/controller/SalesController.java
+++ src/main/java/com/example/demo/controller/SalesController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SalesController {
-
+	
+	@RequestMapping("/sales/top")
+	public String  sales_top(Model m) {
+		return "/sales/top";
+	}
 }
