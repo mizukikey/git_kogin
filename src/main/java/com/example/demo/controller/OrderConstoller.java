3127fae1dae0760a9bd48e81dddddd9ef2c73c94
ia <ia@host.docker.internal>
2026-02-03 16:21:41 +0900

    2026_0203_1621_注文情報削除機能追加

--- src/main/java/com/example/demo/controller/OrderConstoller.java
+++ src/main/java/com/example/demo/controller/OrderConstoller.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dto.SalesViewDto;
 import com.example.demo.model.Entity_customer;
+import com.example.demo.model.SalesService;
 import com.example.demo.repository.SalesRepository;
 
 @Controller
@@ -19,6 +22,9 @@ public class OrderConstoller {
 	
 	@Autowired
 	private SalesRepository salesRepository;
+	
+	@Autowired
+	private SalesService salesService;
 	
 	@RequestMapping("/order/top")
 	public String  order_top(Model m) {
@@ -41,6 +47,48 @@ public class OrderConstoller {
 //	    model.addAttribute("customer", customer);
 	    return "order/show";
 	}
+	
+	@RequestMapping("/order/update")
+	public String  order_update(HttpSession session,Model m) {
+		Entity_customer customer = (Entity_customer) session.getAttribute("loginCustomer");
+	    if (customer == null) {
+	        return "redirect:/customer/login"; // 未ログインならログインページへ
+	    }
+	    // customer_id を使って注文情報を取得
+	    Integer customerId = customer.getId();
+	    List<SalesViewDto> orders = salesRepository.findByCustomerId(customerId);
+	    m.addAttribute("orders", orders);
+		return "/order/update";
+	}
+	
+	
+	@RequestMapping("/order/delete")
+	public String  order_delete(HttpSession session,Model m) {
+		Entity_customer customer = (Entity_customer) session.getAttribute("loginCustomer");
+	    if (customer == null) {
+	        return "redirect:/customer/login"; // 未ログインならログインページへ
+	    }
+	    // customer_id を使って注文情報を取得
+	    Integer customerId = customer.getId();
+	    List<SalesViewDto> orders = salesRepository.findByCustomerId(customerId);
+	    m.addAttribute("orders", orders);
+		return "/order/delete";
+	}
+	
+	@RequestMapping("/order/delete_confirm")
+	public String  delete_confirm(HttpServletRequest req,HttpServletRequest r,HttpSession s, Model m) {
+		int id=Integer.parseInt(req.getParameter("id"));
+	    SalesViewDto dto = salesRepository.findSalesViewById(id);
+	    m.addAttribute("sales", dto);
+		return "/order/delete_confirm";
+	}
+	
+	@RequestMapping("/order/delete_result")
+	public String  delete_result(@RequestParam("id") Integer id) {
+	    salesService.deleteSale(id);
+		return "/order/delete_result";
+	}
+	
 
 
 }
