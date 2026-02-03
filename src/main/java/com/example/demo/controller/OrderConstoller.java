5e7665365002502bc1cad034c970af5bea848a88
ia <ia@host.docker.internal>
2026-02-03 16:32:01 +0900

    2026_0203_1631_注文のinput途中

--- src/main/java/com/example/demo/controller/OrderConstoller.java
+++ src/main/java/com/example/demo/controller/OrderConstoller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -9,11 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dto.SalesViewDto;
+import com.example.demo.model.CustomerService;
 import com.example.demo.model.Entity_customer;
+import com.example.demo.model.Entity_product;
+import com.example.demo.model.ManagerService;
+import com.example.demo.model.ProductService;
 import com.example.demo.model.SalesService;
 import com.example.demo.repository.SalesRepository;
 
@@ -22,6 +28,15 @@ public class OrderConstoller {
 	
 	@Autowired
 	private SalesRepository salesRepository;
+	
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private ManagerService managerService;
+	
+	@Autowired
+	private CustomerService customerService;
 	
 	@Autowired
 	private SalesService salesService;
@@ -46,6 +61,39 @@ public class OrderConstoller {
 
 //	    model.addAttribute("customer", customer);
 	    return "order/show";
+	}
+	
+	@GetMapping("/order/input")
+	public String orderInput(
+	        @ModelAttribute("salesDto") SalesViewDto dto,
+	        HttpSession session,
+	        Model model) {
+
+	    // プルダウン共通
+	    model.addAttribute("productList", productService.findAll());
+//	    model.addAttribute("customerList", customerService.findAll());
+	    model.addAttribute("managerList", managerService.findAll());
+	    // ログイン中の顧客を取得
+	    Entity_customer customer = (Entity_customer) session.getAttribute("loginCustomer");
+	    
+	    if (customer != null) {
+	        // DTO に顧客IDと顧客名をセット
+	        dto.setCustomerId(customer.getId());
+	        dto.setCustomerName(customer.getName());
+	    }
+	    // 数量リスト（商品選択後のみ）
+	    if (dto.getProductId() != null) {
+	        Entity_product product = productService.findById(dto.getProductId());
+	        List<Integer> quantityList = IntStream
+	                .rangeClosed(1, product.getStock())
+	                .boxed()
+	                .toList();
+	        model.addAttribute("quantityList", quantityList);
+	    } else {
+	        model.addAttribute("quantityList", List.of());
+	    }
+
+	    return "order/input";
 	}
 	
 	@RequestMapping("/order/update")
